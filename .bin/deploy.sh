e1422f7e7dfc2d6c5bba828d513949111a06bfca
liuwenjian <liuwenjian@bilibili.com>
2022-06-29 13:38:15 +0800

    添加线上部署配置

---
+++ .bin/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+currentDir=$(dirname $0)
+. $currentDir/log.sh
+function handleresult(){
+  if [ $? -eq 0 ];
+  then
+    log 6 $1;
+  else
+    exit 0;
+  fi
+}
+appname=$1
+folder=$appname
+# 获取containerId
+containerid=`docker ps -a | grep $appname | awk '{print $1}'`
+if [[ ! $containerid ]];
+then
+  echo "$appname is not exists"
+  cd $folder
+  # 编译镜像
+  npm run image
+  handleresult '编译镜像成功'
+  # 发布容器
+  npm run deploy
+  handleresult "${appname}:容器发布成功"
+else
+  # 获取镜像ID
+  # imageid=docker images | grep reactutils | awk '${print $3}'
+  # 停止容器
+  docker stop $containerid
+  handleresult "停止容器${appname}成功，容器id:${containerid}"
+  # 删除容器
+  docker rm $containerid
+  handleresult "删除容器${appname}成功，容器id:${containerid}"
+  # 删除镜像
+  docker images | grep ${appname} | awk '{print $3}' | xargs docker rmi
+  handleresult "删除镜像${appname}成功"
+  cd $folder
+  # 编译镜像
+  npm run image
+  handleresult '编译镜像成功'
+  # 发布容器
+  npm run deploy
+  resid=$(docker ps -a | grep $appname | awk '{print $1}')
+  handleresult "${appname}:容器发布成功,容器id:${resid}"
+fi
---
+++ .bin/init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker create network micro-app
