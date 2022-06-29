d922489646e986617dbb346dfcfa09e3d9b9957a
liuwenjian <liuwenjian@bilibili.com>
2022-06-29 15:10:57 +0800

    修改config配置

--- root/src/config.js
+++ root/src/config.js
@@ -1,6 +1,6 @@
 
 // 开发环境地址
-const config = {
+let config = {
   app1: 'http://localhost:8001',
   app2: 'http://localhost:8002',
   app3: 'http://localhost:8003'
@@ -9,9 +9,14 @@ const config = {
 // 线上环境地址
 if (process.env.NODE_ENV === 'production') {
   // 基座应用和子应用部署在同一个域名下，这里使用location.origin进行补全
-  Object.keys(config).forEach((key) => {
-    config[key] = window.location.origin
-  })
+  // Object.keys(config).forEach((key) => {
+  //   config[key] = window.location.origin
+  // })
+  config = {
+    app1: 'http://liuwenjian.cn:8001',
+    app2: 'http://liuwenjian.cn:8002',
+    app3: 'http://liuwenjian.cn:8003'
+  }
 }
 
 
