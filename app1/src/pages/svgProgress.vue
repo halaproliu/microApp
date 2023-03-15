b0b5ac53a3902c4857eb067d4475a4c3b10f0b02
luffy <luffybryant@gmail.com>
2023-03-15 23:50:02 +0800

    添加svg进度条

---
+++ app1/src/pages/svgProgress.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <svg width="300" height="300">
+      <circle
+        ref="el"
+        class="around"
+        cx="150"
+        cy="150"
+        r="100"
+        fill="transparent"
+        stroke-width="12"
+        stroke="grey"
+      ></circle>
+      <text
+        x="150"
+        y="150"
+        font-size="24"
+        fill="grey"
+        text-anchor="middle"
+        alignment-baseline="middle"
+      >
+        {{ percent }}
+      </text>
+    </svg>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from 'vue';
+export default {
+  setup() {
+    const el = ref(null);
+    let percent = ref(0);
+    const loadingArr = [];
+    for (let i = 1; i <= 100; i++) {
+      loadingArr.push(i);
+    }
+    let index = 0;
+    onMounted(() => {
+      const oLength = el?.value?.getTotalLength();
+      let timer = setInterval(() => {
+        el.value.style['stroke-dashoffset'] =
+          oLength - oLength * (loadingArr[index] / 100);
+        percent.value = `${loadingArr[index]}%`;
+        index++;
+        if (index === loadingArr.length) {
+          clearInterval(timer);
+          // percent.value = '加载完毕';
+        }
+      }, 50);
+    });
+
+    onUnmounted(() => {
+      clearInterval(timer);
+    });
+    return {
+      el,
+      percent,
+    };
+  },
+};
+</script>
+
+<style>
+.around {
+  stroke-dasharray: 628;
+  stroke-dashoffset: 628;
+  transition: all 0.5s;
+}
+</style>
